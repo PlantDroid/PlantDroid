8e28384b88a8746ab86268f4723da5f829d8fbe4
Yelann <61114536+Yelann@users.noreply.github.com>
2021-11-09 00:02:24 +0800

    乱七八糟

--- app/src/main/java/com/example/plantdroid/Activity/BottomNavigation.java
+++ app/src/main/java/com/example/plantdroid/Activity/BottomNavigation.java
@@ -1,14 +1,16 @@
-package com.example.plantdroid;
+package com.example.plantdroid.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.plantdroid.R;
+
+public class BottomNavigation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bottom_navigation);
     }
 }
