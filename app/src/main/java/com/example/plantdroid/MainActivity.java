0538725e8889a0bbab0c7ffd4a001fd9c1eee62e
Yelann <61114536+Yelann@users.noreply.github.com>
2021-11-09 01:03:11 +0800

    Revert "乱七八糟"
    This reverts commit 8e28384b88a8746ab86268f4723da5f829d8fbe4.

--- app/src/main/java/com/example/plantdroid/MainActivity.java
+++ app/src/main/java/com/example/plantdroid/MainActivity.java
@@ -1,16 +1,14 @@
-package com.example.plantdroid.Activity;
+package com.example.plantdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.plantdroid.R;
-
-public class BottomNavigation extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_navigation);
+        setContentView(R.layout.activity_main);
     }
 }
