6fdd2b9deb00a8949cb4d8442f55047525793403
HOLLY <775363056@qq.com>
2021-11-11 14:31:34 +0800

    增加tab效果
    但是有bug，我准备merge一下解决gradle的问题

--- app/src/main/java/com/example/plantdroid/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/plantdroid/ui/notifications/NotificationsFragment.java
@@ -1,22 +1,28 @@
 package com.example.plantdroid.ui.notifications;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
+import androidx.viewpager.widget.ViewPager;
 import com.example.plantdroid.R;
 import com.example.plantdroid.databinding.FragmentNotificationsBinding;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
-
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    //左右滑动的标题
+    private String[] mTitles = {"Angiospermae", "Gymnospermae", "Pteridophyta", "Bryophyta", "Lichens", "Mushrooms", "Algae"};
+    //每个标题对应的Fragment
+    private  List<Fragment> fragments;
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
 
@@ -24,17 +30,20 @@ public class NotificationsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
-
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        fragments = new ArrayList<>();
+        mTabLayout = (TabLayout) root.findViewById(R.id.tablayout);
+        mViewPager = (ViewPager) root.findViewById(R.id.viewpager);
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        for (int i = 0; i < mTitles.length; i++) {
+            Fragment fragment=MainFragment.newInstance(mTitles[i]);
+            fragments.add(fragment);
+        }
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getFragmentManager(), fragments, mTitles);
+        mViewPager.setAdapter(adapter);
+        mTabLayout.setupWithViewPager(mViewPager);
         return root;
     }
 
