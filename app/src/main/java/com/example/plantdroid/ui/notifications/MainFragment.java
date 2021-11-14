65e30c8764cd3d0c50672963363608dced0d077d
HOLLY <775363056@qq.com>
2021-11-15 00:44:20 +0800

    删除不必要的代码

--- app/src/main/java/com/example/plantdroid/ui/notifications/MainFragment.java
+++ app/src/main/java/com/example/plantdroid/ui/notifications/MainFragment.java
@@ -21,14 +21,11 @@ import com.example.plantdroid.R;
 
 @SuppressLint("ValidFragment")
 public class MainFragment extends Fragment {
-    private static final String TAG = "!!!";
-    TextView mTextView;
 
     public static MainFragment newInstance(String title) {
         MainFragment mainFragment = new MainFragment();
         Bundle bundle = new Bundle();
         bundle.putString("title", title);
-        Log.e(TAG, "onCreateView: " +title);
         mainFragment.setArguments(bundle);
         return mainFragment;
     }
@@ -36,29 +33,6 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view, container, false);
-        Log.e(TAG, "onCreateView: !!!!" );
-//        view.setBackground(new Drawable() {
-//            @Override
-//            public void draw(@NonNull Canvas canvas) {
-//                canvas.drawColor(Color.BLACK);
-//            }
-//
-//            @Override
-//            public void setAlpha(int i) {
-//
-//            }
-//
-//            @Override
-//            public void setColorFilter(@Nullable ColorFilter colorFilter) {
-//
-//            }
-//
-//            @SuppressLint("WrongConstant")
-//            @Override
-//            public int getOpacity() {
-//                return 0;
-//            }
-//        });
         return view;
     }
 }
