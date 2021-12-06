33bfdc87cf610e794f4f52d7f8bba8b6a31b6e57
Sunnary2604 <50614684+Sunnary2604@users.noreply.github.com>
2021-12-06 18:45:37 +0800

    map前一页

--- app/src/main/java/com/example/plantdroid/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/plantdroid/ui/dashboard/DashboardViewModel.java
@@ -3,17 +3,19 @@ package com.example.plantdroid.ui.dashboard;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.plantdroid.Database.PlantDroidViewModel;
 
 public class DashboardViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
     public DashboardViewModel() {
+
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment55");
-
     }
-
 
     public LiveData<String> getText() {
         return mText;
