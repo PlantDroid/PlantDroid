921aa0bf09d05b0cb4208c97f8a08bb6e6c5d0c7
Focus <8004930-FooxKing@users.noreply.gitlab.com>
2021-12-08 23:16:36 +0800

    update prefill_database.db

--- app/src/main/java/com/example/plantdroid/Database/PlantDroidDatabase.java
+++ app/src/main/java/com/example/plantdroid/Database/PlantDroidDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Plant.class, DiscoveredPlant.class}, version = 4, exportSchema = false)
+@Database(entities = {Plant.class, DiscoveredPlant.class}, version = 5, exportSchema = false)
 public abstract class PlantDroidDatabase extends RoomDatabase {
     private static PlantDroidDatabase INSTANCE;
 
